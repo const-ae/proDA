a8467e11178cc763ec01ffe1f0a3e1084d69ae24
Thomas Naake <thomasnaake@gmx.de>
2021-01-12 09:12:42 +0100

    rewrite dt.scaled

--- R/stats_functions.R
+++ R/stats_functions.R
@@ -2,13 +2,14 @@
 
 
 
-dt.scaled <- function(x, df, mean=0, sd=1, log=FALSE){
-  res <- dt((x-mean)/sd, df=df, log=log)
-  if(log){
-    res - log(sd)
-  }else{
-    res * 1/sd
-  }
+dt.scaled <- function (x, df, mean = 0, sd = 1, log = FALSE) {
+    res <- dt((x - mean) / sd, df = df, log = log)
+    if (log) {
+        res - log(sd)
+    }
+    else {
+        res / sd
+    }
 }
 
 
